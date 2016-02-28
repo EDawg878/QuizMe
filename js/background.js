c032803c1c2f720f02877365db94275a50eeca8e
Jason <jayxuchen@gmail.com>
2016-02-28 10:51:15 -0500

    fixes button

--- js/background.js
+++ js/background.js
@@ -11,56 +11,65 @@ var totalSet = [];
 
 // QUIZ
 function read(data) {
-  data.split("\n").forEach(function(line) {
-    var entry = line.split("|");
-    var q = entry[0];
-    var a = entry[1];
-    totalSet.push([q, a]);
-  });
+    data.split("\n").forEach(function (line) {
+        var entry = line.split("|");
+        var q = entry[0];
+        var a = entry[1];
+        totalSet.push([q, a]);
+    });
 }
 
 function populateSet(currSet) {
-  var n = 0;
-  while (n < nQuestions) {
-    if (qIndex == totalSet.length) qIndex = 0;
-    var arr = totalSet[qIndex];
-    currSet.push(arr);
-    qIndex++;
-    n++;
-  }
+    var n = 0;
+    while (n < nQuestions) {
+        if (qIndex == totalSet.length) qIndex = 0;
+        var arr = totalSet[qIndex];
+        currSet.push(arr);
+        qIndex++;
+        n++;
+    }
 }
 
 $.get("quiz.txt", read);
 //
-
+// sets default icon imag
+chrome.browserAction.setIcon({
+    path: "/img/enabled.jpg"
+});
 // Background
 function studyBreak(tab) {
-  chrome.tabs.update(tab.id, { url : "popup.html" });
+    chrome.tabs.update(tab.id, {
+        url: "popup.html"
+    });
 }
 
 function log(url, tab) {
-  if (enabled && ++count >= threshold) {
-    destURL = url;
-    studyBreak(tab);
-    count = 0;
-  }
+    if (enabled && ++count >= threshold) {
+        destURL = url;
+        studyBreak(tab);
+        count = 0;
+    }
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  var url = changeInfo.url;
-  if (url) {
-    log(url, tab);
-  }
-})
-//
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        var url = changeInfo.url;
+        if (url) {
+            log(url, tab);
+        }
+    })
+    //
 
 // Toggle Button
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if (enabled) {
-    chrome.browserAction.setIcon({path : "/img/disabled.jpg", tabId : tab.id});
-    enabled = false; 
-  } else {
-    chrome.browserAction.setIcon({path : "/img/enabled.jpg", tabId : tab.id});
-    enabled = true;
-  }
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (enabled) {
+        chrome.browserAction.setIcon({
+            path: "/img/disabled.jpg"
+        });
+        enabled = false;
+    } else {
+        chrome.browserAction.setIcon({
+            path: "/img/enabled.jpg"
+        });
+        enabled = true;
+    }
 });
